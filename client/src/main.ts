26076a8eefcc0e9daca555835d308a03d83043c5
wellinton <wellinton.camboim@gmail.com>
2021-03-15 22:32:41 -0300

    Estrutura inicial do client-Front-end

---
+++ client/src/main.ts
@@ -0,0 +1,5 @@
+// IMPORTA O ARQUIVO DE ESTILO PADRÃO DA NOSSA APLICAÇÃO
+import "./main.scss";
+
+// IMPRIME `Olá mundo do main.ts ...` NO CORPO DA APLICAÇÃO
+document.body.innerHTML = "Olá mundo do main.ts ... ";
